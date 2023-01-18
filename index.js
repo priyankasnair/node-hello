4adf7c26d07a81387787dad1714c6478b5974351
priyankasnair <priyankasnair12@gmail.com>
2023-01-18 14:15:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+// const http = require('http');
+// const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   const msg = 'Hello Node!\n'
+//   res.end(msg);
+// });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+// server.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}/`);
+// });
+
+console.log('hello,demo');
+
+var moment = require('moment');
+var date = moment().format('LL');
+console.log(date);
+
